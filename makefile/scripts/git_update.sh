3f26c18770841016d963495bd2e245bcceb0c70a
David Carver <d_a_carver@yahoo.com>
2014-07-31 15:18:22 -0400

    Import ADT for Eclipse and rename to Android Core

--- makefile/scripts/git_update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-GIT_BRANCH=`git branch | grep "*" | cut -b 3-`
-if [ x$GIT_BRANCH != "x(no branch)" ]
-then
-	git pull origin $GIT_BRANCH
-else
-	echo "No branch set: Have you checked out a branch?"
-fi
